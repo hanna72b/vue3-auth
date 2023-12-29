c1ae2d03680feb8771dd00e9a6421a50e66605dd
hanna72b <hannah1993bb@gmail.com>
2023-12-29 20:20:58 +0330

    2

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home 1</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">About2</RouterLink>
       </nav>
     </div>
   </header>
